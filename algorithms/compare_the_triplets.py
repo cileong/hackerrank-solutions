63419a68d57742aff128746396aa63876c426ff0
Bill Ong <ongcileong2001@gmail.com>
2021-07-13 19:57:48 +0800

    Add solution - Compare the Triplets

---
+++ algorithms/compare_the_triplets.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice, bob = 0, 0
+    for i, j in zip(a, b):
+        if i > j:
+            alice += 1
+        elif i < j:
+            bob += 1
+    return [alice, bob]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
